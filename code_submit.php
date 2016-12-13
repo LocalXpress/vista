ccd4a7964b733d62a99f8763d05682d973131344
Swaroop Acharjee <meet.ripon@gmail.com>
2016-12-13 23:24:21 +0530

    Java file added

--- code_submit.php
+++ code_submit.php
@@ -19,11 +19,12 @@
 	echo '<pre>'.htmlspecialchars($output,ENT_QUOTES).'</pre>';//This part is left for the comparison.
 	unlink($tempfile);
 	*/
-	
+	//C part
+	/*
 	$tempfile = $file.'.c';
 	$exefile = $file.'.exe';
 
- //String code made for C++ 
+ //String code made for C
  $code="#include".'"stdio.h"'.PHP_EOL.$_REQUEST["c_code"]."int main(){printf(".'"%d"'.",add(4,5));return 0;}";//problems with left shift operator
 	//echo $code;
 	
@@ -33,4 +34,27 @@
   
 	echo '<pre>'.htmlspecialchars($output,ENT_QUOTES).'</pre>';//This part is left for the comparison.
 	unlink($tempfile);
+	*/
+ //String code made for C
+	$JAVA_HOME = "\jre1.8.0_45";
+	$PATH = "$JAVA_HOME/bin:".getenv('PATH');
+	putenv("JAVA_HOME=$JAVA_HOME");
+	putenv("PATH=$PATH");
+$code="class ".$file." {".$_REQUEST["c_code"]."public static void main(String[] args)throws Exception{System.out.println(add(5,4));}}";
+//echo $code;
+	
+	file_put_contents($file.'.java', $code);
+	$com_1='javac '.$file.'.java';
+	$com_2='java '.$file;
+	echo "<br>".$com_1."<br>".$com_2."<br>";
+	$output=shell_exec($com_1." 2>&1");
+	echo $output."<br>";
+	//$output = shell_exec(escapeshellarg($com_2));
+	//echo realpath('Hello.java');
+	$output = shell_exec($com_2." 2>&1");
+	//$output=shell_exec(escapeshellarg('java Hello'));
+  
+	echo '<pre>'.htmlspecialchars($output,ENT_QUOTES).'</pre>';//This part is left for the comparison.
+	//unlink($tempfile);
+	
 ?>
