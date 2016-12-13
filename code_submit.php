d3b0e85f7e4d073618545a7e97eac14d651290d6
Swaroop Acharjee <meet.ripon@gmail.com>
2016-12-13 11:02:14 +0530

    The file has only be written for C++

---
+++ code_submit.php
@@ -0,0 +1,19 @@
+<?php 
+	$tempfile = tempnam('', 'source');
+	//$ques_no=$_GET["ques"];//variable to get the question number
+	$file=$_REQUEST["name_code"];//the name of the file will name_1+name_2+unique_id
+	//echo $file;
+	$tempfile = $file.'.cpp';
+	$exefile = $file.'.exe';
+
+ //String code made for C++ 
+ $code="#include".'"iostream"'.PHP_EOL." #include".'"stdio.h"'.PHP_EOL."using namespace std;".$_REQUEST["c_code"]."int main(){printf(".'"%d"'.",add(4,5));return 0;}";//problems with left shift operator
+	//echo $code;
+	
+	file_put_contents($tempfile, $code);
+	$output = shell_exec('g++ '.escapeshellarg($tempfile).' -o Codes/'.escapeshellarg($exefile));
+	$output = shell_exec(escapeshellarg('Codes/'.$exefile));
+  
+	echo '<pre>'.htmlspecialchars($output,ENT_QUOTES).'</pre>';//This part is left for the comparison.
+	unlink($tempfile);
+?>
