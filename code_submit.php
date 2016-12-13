2395cb4158511b037fbce90b7d690577ee29af73
Swaroop Acharjee <meet.ripon@gmail.com>
2016-12-13 21:14:29 +0530

    C part is added

--- code_submit.php
+++ code_submit.php
@@ -1,5 +1,4 @@
 <?php 
-	<?php 
 	$tempfile = tempnam('', 'source');
 	//$ques_no=$_GET["ques"];//variable to get the question number
 	$file=$_REQUEST["name_code"];//the name of the file will name_1+name_2+unique_id
@@ -25,14 +24,13 @@
 	$exefile = $file.'.exe';
 
  //String code made for C++ 
- $code="#include".'"stdio.h"'.PHP_EOL."using namespace std;".$_REQUEST["c_code"]."int main(){printf(".'"%d"'.",add(4,5));return 0;}";//problems with left shift operator
+ $code="#include".'"stdio.h"'.PHP_EOL.$_REQUEST["c_code"]."int main(){printf(".'"%d"'.",add(4,5));return 0;}";//problems with left shift operator
 	//echo $code;
 	
 	file_put_contents($tempfile, $code);
-	$output = shell_exec('g++ '.escapeshellarg($tempfile).' -o Codes/'.escapeshellarg($exefile));
+	$output = shell_exec('gcc '.escapeshellarg($tempfile).' -o Codes/'.escapeshellarg($exefile));
 	$output = shell_exec(escapeshellarg('Codes/'.$exefile));
   
 	echo '<pre>'.htmlspecialchars($output,ENT_QUOTES).'</pre>';//This part is left for the comparison.
 	unlink($tempfile);
-?>
 ?>
