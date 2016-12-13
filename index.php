777deeb678c14a16d452611d5f2cac5807011440
Swaroop Acharjee <meet.ripon@gmail.com>
2016-12-13 23:25:09 +0530

    Unknown changes

--- index.php
+++ index.php
@@ -5,11 +5,10 @@
     <title>Code Compiler</title>
   </head>
   <body>
-
-    <form action="code_submit.php" method="POST">
+<form action="code_submit.php" method="POST">
 <input type="text" id="name_code" name="name_code">
 <textarea name="c_code" id="c_code" style="float: right; height: 500px; width:500px; background: black; color:green; font-size:20px; font-type:CONSOLAS;"></textarea>
-      <button type="submit">Submit</button>
+<button type="submit">Submit</button>
     </form>
   </body>
 </html>
