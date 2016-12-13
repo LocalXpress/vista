0d4f187619c9f29e04c740128e8566c6f5b49d07
Swaroop Acharjee <meet.ripon@gmail.com>
2016-12-13 11:02:57 +0530

    Without UI

--- index.php
+++ index.php
@@ -1,6 +1,15 @@
-<?php
-$output = shell_exec('kmp.exe');
-echo "<pre>$output</pre>";
-$output = shell_exec('java Factorial');
-echo "<pre>$output</pre>";
-?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Code Compiler</title>
+  </head>
+  <body>
+
+    <form action="code_submit.php" method="POST">
+<input type="text" id="name_code" name="name_code">
+<textarea name="c_code" id="c_code" style="height: 500px; width:500px; background: black; color:green; font-size:20px; font-type:CONSOLAS;"></textarea>
+      <button type="submit">Submit</button>
+    </form>
+  </body>
+</html>
